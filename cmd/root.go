aba67a64d84600d64459213828b87bc3dd151454
mickamy <tetsuro@mickamy.com>
2025-04-01 17:42:59 +0900

    feat: add option to execute formatter on generate

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 
@@ -13,7 +14,10 @@ import (
 	"github.com/mickamy/gon/cmd/version"
 )
 
-var versionFlag bool
+var (
+	versionFlag bool
+	formatOpt   string
+)
 
 var Cmd = &cobra.Command{
 	Use:   "gon",
@@ -25,9 +29,17 @@ var Cmd = &cobra.Command{
 			os.Exit(0)
 		}
 	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if err := runFormatting(formatOpt); err != nil {
+			fmt.Printf("⚠️ Failed to format code: %v\n", err)
+		}
+	},
 }
 
 func init() {
+	Cmd.PersistentFlags().BoolVarP(&versionFlag, "version", "v", false, "Print the version and exit")
+	Cmd.PersistentFlags().StringVar(&formatOpt, "format", "none", "Format generated code (none, gofmt, goimports, all)")
+
 	Cmd.AddCommand(destroy.Cmd)
 	Cmd.AddCommand(generate.Cmd)
 	Cmd.AddCommand(initCmd.Cmd)
@@ -40,4 +52,29 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func runFormatting(opt string) error {
+	switch opt {
+	case "gofmt":
+		return runCommand("go", "fmt", "./...")
+	case "goimports":
+		return runCommand("goimports", "-w", ".")
+	case "all":
+		if err := runCommand("go", "fmt", "./..."); err != nil {
+			return err
+		}
+		return runCommand("goimports", "-w", ".")
+	case "none":
+		return nil
+	default:
+		return fmt.Errorf("unknown format option: %s", opt)
+	}
+}
+
+func runCommand(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
--- example/internal/domain/user/repository/user_repository.go
+++ example/internal/domain/user/repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"gorm.io/gorm"
 

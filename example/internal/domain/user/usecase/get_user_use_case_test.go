76a7021a7d61a9d33651884f3a2bc115518c9050
mickamy <tetsuro@mickamy.com>
2025-04-01 19:08:50 +0900

    chore: generate use-case test in example

---
+++ example/internal/domain/user/usecase/get_user_use_case_test.go
@@ -0,0 +1,25 @@
+package usecase_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/mickamy/gon-example/internal/domain/user/usecase"
+)
+
+func TestGetUser_Do(t *testing.T) {
+	t.Parallel()
+
+	// arrange
+	ctx := t.Context()
+
+	// act
+	sut := usecase.NewGetUser()
+	got, err := sut.Do(ctx, usecase.GetUserInput{})
+
+	// assert
+	require.NoError(t, err)
+	assert.Equal(t, usecase.GetUserOutput{}, got)
+}

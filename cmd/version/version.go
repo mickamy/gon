9918a89002534e94151ca375c25e27dc77008b09
mickamy <tetsuro@mickamy.com>
2025-04-01 16:55:20 +0900

    feat: print that the app was built from source

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -8,11 +8,18 @@ import (
 
 var Version = "dev"
 
+func String() string {
+	if Version == "dev" {
+		return "dev (built from source; try GitHub release for accurate version)"
+	}
+	return Version
+}
+
 var Cmd = &cobra.Command{
 	Use:     "version",
 	Aliases: []string{"v"},
 	Short:   "Show gon version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("gon version %s\n", Version)
+		fmt.Printf("gon version %s\n", String())
 	},
 }
